290e877044507bfdd84be3304e943d5e0ceefa6d
htrillo-code <h.trillo@alumno.etec.um.edu.ar>
2026-04-27 08:36:55 -0300

    este es el https

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -22,4 +22,5 @@ fun main() {
     
     complejo.valor(3, 4)      // Le das los valores
     println(complejo.mostrar()) // ¡Imprimes el resultado!
+    println("hola paput") // ¡Imprimes el resultado!
 }
