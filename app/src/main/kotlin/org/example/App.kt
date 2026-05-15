659d0d9eef1967c69f1b818de527eeb80219fbec
htrillo-code <h.trillo@alumno.etec.um.edu.ar>
2026-05-15 08:58:57 -0300

    nuevocontodo

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,7 @@
 package org.example
 
-class Complejo(private var real: Int=0, private var imag: Int=0) {
-    fun inicializar(real:Int,imag:Int){
+class Complejo(private var real: Double=0.0, private var imag: Double=0.0) {
+    fun inicializar(real:Double,imag:Double){
         this.real=real
         this.imag=imag
     }
@@ -17,15 +17,31 @@ class Complejo(private var real: Int=0, private var imag: Int=0) {
             imag - otro.imag
         )
     }
+    fun multiplicar(otro: Complejo): Complejo {
+            val r =  real * otro.real - (this.imag*otro.imag)
+            val i = real * otro.imag +(imag * otro.real)
+            return Complejo(r,i)
+    }
+        fun dividir(otro: Complejo): Complejo {
+            val denominador= otro.real*otro.real + (otro.imag*otro.imag)
+            val r = (real * otro.real + (this.imag*otro.imag)) / denominador
+            val i = (this.imag * otro.real - (this.real * otro.imag)) /denominador
+
+            return Complejo(r,i)
+    }
 }
 
 fun main() {
     var complejo: Complejo
     complejo= Complejo()
-    complejo.inicializar(3,4)
+    complejo.inicializar(3.0,4.0)
     println("mi número complejo es ${complejo.toString()}")
-    var c2:Complejo= Complejo(1,2)
+    var c2:Complejo= Complejo(1.0,2.0)
     var c3:Complejo
     c3=complejo.restar(c2)
     c3.mostrar()
+    var c4=complejo.multiplicar(c2)
+    c4.mostrar()
+    var c5=complejo.dividir(c2)
+    c5.mostrar()
 }
